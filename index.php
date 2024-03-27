0f9199ec0c3b4c695ff596abcb61143ed695d6af
rupom <mohrajulrupom@gmail.com>
2024-03-27 13:56:08 +0600

    page colunm management

--- index.php
+++ index.php
@@ -6,5 +6,20 @@
  * Version: 1.0
  *
  */
-
+function clm_manage_column($columns){
+    unset($columns['comments']);
+    $columns['id'] = 'Post Id';
+    $columns['thumbnail'] = 'Thumbnail';
+    return $columns;
+}
+ add_filter( 'manage_pages_columns', 'clm_manage_column');
+ function clm_custom_column($column,$post_id){
+    if('id' == $column){
+        echo $post_id;
+    }elseif('thumbnail' == $column){
+        $thumbnail = get_the_post_thumbnail($post_id, array(100,50));
+        echo $thumbnail;
+    }
+ }
+ add_action( 'manage_pages_custom_column', 'clm_custom_column',10,2);
 ?>
